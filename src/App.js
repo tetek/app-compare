27d02a3426069ad0d3e3e606d1ad631d71af923f
Wojtek Mandrysz <tetek1@gmail.com>
2022-11-24 08:25:06 +0100

    it's coming together

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 // import logo from './logo.svg';
-import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 // import React from 'react'
 // import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import SankeyChart from './components/sankeyChart'
+import SankeyChart from "./components/sankeyChart";
 function App() {
   return (
     <div className="App">
-      <SankeyChart />
+      <div className="container">
+      <div className="row">.</div>
+        <div className="row justify-content-md-center">
+          <h1>hey</h1>
+        </div>
+        <div className="row">
+          <div className="col">1Password 1.0</div>
+          <div className="col-md-auto">
+            {/* Variable width content */}
+            <SankeyChart />
+          </div>
+          <div className="col col-lg-2">1Password 2.0</div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-export default App
+export default App;
